10d5834dab2e6c77e7c88a2e6bcab3f3f7c85faa
Cowpooke <massterx9@gmail.com>
2024-06-09 01:07:07 +0700

    fix

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -25,6 +25,6 @@ class RegisterController extends Controller
         ]);
 
         Session::flash('message', 'Register Berhasil. Akun Anda sudah Aktif silahkan Login menggunakan username dan password.');
-        return redirect('login');
+        return redirect()->route('login');
     }
 }
