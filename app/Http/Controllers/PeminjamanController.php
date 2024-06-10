fc6dfd3924bee4bcd93c48b5dffcd7ae24d03389
Cowpooke <massterx9@gmail.com>
2024-06-10 13:29:04 +0700

    fix

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class PeminjamanController extends Controller
 {
-    public function getData(Request $request)
+    public function getData()
     {
-        $user = $request->Auth::user()->username;  
+        $user = Auth::user()->username;  
         $peminjaman = DB::table('peminjaman')
         ->join('bukus', 'id', '=', 'bukus.idbuku')
         ->select('bukus.idbuku','bukus.judul', 'peminjaman.tgl_pinjam','peminjaman.tgl_kembali','peminjaman.status')
