44756ed66c95e8427a5998b0f2a6f754b138d1c3
Cowpooke <massterx9@gmail.com>
2024-06-10 13:24:40 +0700

    .

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -7,12 +7,13 @@ use Illuminate\Support\Facades\DB;
 
 class PeminjamanController extends Controller
 {
-    public function getData()
+    public function getData(Request $request)
     {
+        $user = $request->Auth::user()->username;  
         $peminjaman = DB::table('peminjaman')
         ->join('bukus', 'id', '=', 'bukus.idbuku')
         ->select('bukus.idbuku','bukus.judul', 'peminjaman.tgl_pinjam','peminjaman.tgl_kembali','peminjaman.status')
-        ->where('peminjaman.status','=','aktif')
+        ->where('peminjaman.status','=','aktif','AND','peminjaman.user_id','=',$user)
         ->get();
         return view('peminjaman',['peminjaman'=> $peminjaman]);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 @extends('master')
 
 @section('konten')
-  <h4>Selamat Datang <b>{{Auth::user()->name}}</b>, Anda Login sebagai <b>{{Auth::user()->role}}</b>.</h4>
+  <h4>Selamat Datang <b>{{Auth::user()->username}}</b>, Anda Login sebagai <b>{{Auth::user()->role}}</b>.</h4>
   <p>Cari Data Buku :</p>
     <form class="d-flex ms-2" action="{{ route('search') }}" method="GET">
       <input type="text" name="search" required placeholder="cari buku"/>
