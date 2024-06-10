681c01c6c2cf96895d8c00912da403d321139f15
Cowpooke <massterx9@gmail.com>
2024-06-10 13:32:16 +0700

    .

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -4,17 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
 
 class PeminjamanController extends Controller
 {
     public function getData()
     {
-        $user = Auth::user()->username;  
         $peminjaman = DB::table('peminjaman')
         ->join('bukus', 'id', '=', 'bukus.idbuku')
         ->select('bukus.idbuku','bukus.judul', 'peminjaman.tgl_pinjam','peminjaman.tgl_kembali','peminjaman.status')
-        ->where('peminjaman.status','=','aktif','AND','peminjaman.user_id','=',$user)
+        ->where('peminjaman.status','=','aktif')
         ->get();
         return view('peminjaman',['peminjaman'=> $peminjaman]);
     }
